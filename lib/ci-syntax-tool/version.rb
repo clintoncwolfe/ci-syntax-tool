0a712e0e0e3d1d785bc781a0a89e5cb8dd52789c
Clinton Wolfe <clinton@omniti.com>
2015-11-05 14:22:56 -0500

    Version bump

--- lib/ci-syntax-tool/version.rb
+++ lib/ci-syntax-tool/version.rb
@@ -4,7 +4,7 @@ module CI
     #   Simple container for namespacing the project.  Here, just the
     # version string.
     module Tool
-      VERSION = '0.1.0'
+      VERSION = '0.2.0'
     end
   end
 end
